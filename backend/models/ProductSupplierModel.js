ae2f25872084d32a07050d8e742150887fdf96c8
Xio41 <scjc4190@gmail.com>
2025-07-23 14:55:35 -0600

    Se corrigio el modelo de ProductSupplier

--- backend/models/ProductSupplierModel.js
+++ backend/models/ProductSupplierModel.js
@@ -6,6 +6,19 @@ import Suppliers from "./SupplierModel.js"
 const {DataTypes} = Sequelize;
 
 const ProductSupplier = db.define('product_supplier', {
+    id_product_supplier: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    uuid: {
+        type: DataTypes.STRING,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
     price: {
         type: DataTypes.DECIMAL(10, 2),
         allowNull: true
@@ -13,14 +26,21 @@ const ProductSupplier = db.define('product_supplier', {
     delivery_time: {
         type: DataTypes.INTEGER, // días de entrega
         allowNull: true
-    }
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    supplier_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
 }, {
     freezeTableName: true,
 });
 Products.belongsToMany(Suppliers, {
     through: ProductSupplier,
     foreignKey: 'product_id',
-    otherKey: 'supplier_id',
     onDelete: 'CASCADE', // Si eliminas producto, se borran sus relaciones
     onUpdate: 'CASCADE'
 });
@@ -28,7 +48,6 @@ Products.belongsToMany(Suppliers, {
 Suppliers.belongsToMany(Products, {
     through: ProductSupplier,
     foreignKey: 'supplier_id',
-    otherKey: 'product_id',
     onDelete: 'CASCADE', // Si eliminas proveedor, se borran sus relaciones
     onUpdate: 'CASCADE'
 });
