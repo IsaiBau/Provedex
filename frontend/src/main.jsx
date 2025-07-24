607faeea5977f8d1cbf9d26a8b14b1ebdc361358
isaibau <isaisabi02@gmail.com>
2025-07-24 17:26:47 -0600

    Se anadieron las rutas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import axios from 'axios';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import { store } from './app/store';
+import './index.css';
+import ScrollToTop from './features/ScrollToTop';
+axios.defaults.withCredentials = true;
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <BrowserRouter>
+      <ScrollToTop/>
+      <App/>
+    </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
